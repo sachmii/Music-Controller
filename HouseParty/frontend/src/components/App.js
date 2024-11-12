ed83ad778ad1b336803adfd6001d45e0a4a67904
sachmiii <sehmuel.wagner@tum.de>
2024-11-12 21:32:26 +0100

    New endpoint for Joining Rooms, page design for RoomJoin

--- HouseParty/frontend/src/components/App.js
+++ HouseParty/frontend/src/components/App.js
@@ -1,19 +1,20 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from './HomePage';
+import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (<div> 
-            <HomePage />
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="center">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
-const appDiv = document.getElementById("app")
+const appDiv = document.getElementById("app");
 render(<App />, appDiv);
--- HouseParty/frontend/src/components/RoomJoinPage.js
+++ HouseParty/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,61 @@
 import React, { Component } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: "",
+      error: "hello",
+    };
 
-    render() {
-        return <p>Here you can join a room</p>
-    }
+    this._handleTextFieldChange = this._handleTextFieldChange.bind(this);
+    this._roomButtonPressed = this._roomButtonPressed.bind(this);
+  }
+
+  render() {
+    return (
+      <Grid container spacing={1} align="center">
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h4">
+            Join a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            error={this.state.error}
+            label="Code"
+            placeholder="Enter a Room Code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+            onChange={this._handleTextFieldChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this._roomButtonPressed}
+          >
+            Enter Room
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  _handleTextFieldChange(e) {
+    this.setState({
+      roomCode: e.target.value,
+    });
+  }
+
+  _roomButtonPressed() {}
 }
