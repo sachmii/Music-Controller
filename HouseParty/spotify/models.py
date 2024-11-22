e03281fe7cb6a8f0e7a55be63b9d02e216ec7199
sachmiii <sehmuel.wagner@tum.de>
2024-11-22 15:26:53 +0100

    added docker

--- HouseParty/spotify/models.py
+++ HouseParty/spotify/models.py
@@ -7,7 +7,7 @@ class SpotifyToken(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     refresh_token = models.CharField(max_length=150)
     access_token = models.CharField(max_length=150)
-    expires_in = models.DateTimeField(default=timezone.now())
+    expires_in = models.DateTimeField(default=timezone.now)
     token_type = models.CharField(max_length=50)
 
 class Vote(models.Model):
